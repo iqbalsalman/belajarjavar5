dff82b4e935920dc5d967ef31f410126fe80f17a
iqbalsalman <iqbalsalman1707@gmail.com>
2018-01-09 13:20:29 +0700

    Array List

---
+++ src/ArrayFwList.java
@@ -0,0 +1,22 @@
+package belajarjava5.bin;;
+
+import java.util.List;
+import java.util.ArrayList;
+import java.math.BigDecimal;
+
+public class ArrayFwList{
+	public static void main(String[] args){
+		List values = new ArrayList();
+		values.add("Dimas Maryanto");
+		values.add(1);
+		values.add(true);
+		values.add('A');
+		values.add(100f);
+		values.add(0,1d);
+		values.add(new BigDecimal(1000000));
+
+		for(int i =0; i<values.size(); i++){
+			System.out.println(values.get(i));
+		}
+	}
+}
